6d287d9448c80632b4389e107b44ff188314288b
Your Name <you@example.com>
2026-08-08 20:23:21 +0300

    origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://employees-management-system-w71u.vercel.app",
+  "https://employees-management-system-7oll-8hcfzf7pf.vercel.app/",
 ];
 
 app.use(
